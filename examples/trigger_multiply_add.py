f54873a8d18a0508ca03e6e182991a09071aa7b7
Alexander Goscinski <alex.go@posteo.de>
2025-10-30 14:31:17 +0100

    multiply add

---
+++ examples/trigger_multiply_add.py
@@ -0,0 +1,17 @@
+# Create Process
+from airflow_provider_aiida.aiida_core.engine.launch import submit
+from aiida.workflows.arithmetic.multiply_add import MultiplyAddWorkChain 
+from aiida import load_profile
+load_profile()
+from aiida.orm import load_code, Int
+code = load_code('bash@localhost')
+inputs = {
+    'code': code,
+    'x': Int(0),
+    'y': Int(1),
+    'z': Int(2),
+}
+
+dag_run = submit(MultiplyAddWorkChain, inputs)
+
+print(f"Triggered DAG run: {dag_run}")
