7dce552e80e7571b1f43d31bb2d4c57ac4bf9d57
Alexander Goscinski <alex.go@posteo.de>
2025-10-30 11:38:22 +0100

    add

--- src/airflow_provider_aiida/xcom/backend.py
+++ src/airflow_provider_aiida/xcom/backend.py
@@ -1,5 +1,6 @@
 from airflow.sdk.execution_time.xcom import BaseXCom
 
 class AiidaBackend(BaseXCom):
+    # TODO this should add logic serialize so it can be tracked in the provenance graph
     pass
 
