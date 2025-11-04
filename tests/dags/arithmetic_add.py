937b1986b19822368d1d64f00665f3647762e2a1
Alexander Goscinski <alex.go@posteo.de>
2025-11-04 17:30:32 +0100

    Remove test from older approach
    These were copied over by mistake

--- tests/dags/arithmetic_add.py
+++
@@ -1,8 +0,0 @@
-# TODO load dag from example_dag
-
-dag.test(
-    run_conf={"machine": "localhost",
-              "local_workdir": "/Users/alexgo/code/airflow/local_workdir",
-              "remote_workdir": "/Users/alexgo/code/airflow/remote_workdir",
-              }
-)
